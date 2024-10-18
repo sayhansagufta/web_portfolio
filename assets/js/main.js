a0c475b5df2e2f1a3f97d9b281b324a73753a1bb
Shayhan Sagufta <sayhansagufta99@gmail.com>
2024-10-18 16:04:03 +0700

    menambahkan website portfolio sampai section services

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,93 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav__link");
 
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName("skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header");
 
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
 
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    // Menghilangkan kelas aktif dari semua tab konten
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("qualification__active");
+    });
+
+    // Menambahkan kelas aktif ke target tab konten
+    target.classList.add("qualification__active");
+
+    // Menghilangkan kelas aktif dari semua tab
+    tabs.forEach((tab) => {
+      // Ganti 'tab.forEach' dengan 'tabs.forEach'
+      tab.classList.remove("qualification__active");
+    });
+
+    // Menambahkan kelas aktif ke tab yang di-klik
+    tab.classList.add("qualification__active");
+  });
+});
 
 /*==================== SERVICES MODAL ====================*/
 
 
-/*==================== PORTFOLIO SWIPER  ====================*/
 
+/*==================== PORTFOLIO SWIPER  ====================*/
 
 /*==================== TESTIMONIAL ====================*/
 
-
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
